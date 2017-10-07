13bf74c9181f9ac79967f5d1f3332faef834238d
Samuel Ainsworth <skainsworth@gmail.com>
2017-10-06 18:51:13 -0700

    fix the sampling to respect A

--- main.py
+++ main.py
@@ -68,9 +68,9 @@ np.random.seed(0)
 torch.manual_seed(0)
 
 n_steps = 1000
-true_stddev_emission = 1
-true_stddev_transition = 3
-true_A = 0.7
+true_stddev_emission = 0.05
+true_stddev_transition = 0.1
+true_A = 0.99
 
 def normal_log_prob(x, mu, sigma):
   return -0.5 * (((x - mu) / sigma) ** 2)
@@ -78,11 +78,18 @@ def normal_log_prob(x, mu, sigma):
 # The parameters of our model that will be fit along with the filtering process
 # variational_log_stddev_transition = Variable(torch.randn(1), requires_grad=True)
 # variational_log_stddev_emission = Variable(torch.randn(1), requires_grad=True)
-variational_A = Variable(torch.randn(1), requires_grad=True)
+# variational_A = Variable(torch.randn(1), requires_grad=True)
 
-variational_log_stddev_transition = Variable(torch.log(torch.FloatTensor([true_stddev_transition])), requires_grad=True)
-variational_log_stddev_emission = Variable(torch.log(torch.FloatTensor([true_stddev_emission])), requires_grad=True)
+variational_log_stddev_transition = Variable(
+  torch.log(torch.FloatTensor([true_stddev_transition])),
+  requires_grad=False
+)
+variational_log_stddev_emission = Variable(
+  torch.log(torch.FloatTensor([true_stddev_emission])),
+  requires_grad=False
+)
 # variational_A = Variable(torch.FloatTensor([true_A]), requires_grad=True)
+variational_A = Variable(torch.FloatTensor([0.7]), requires_grad=True)
 
 def trans_lp(x_t, x_prev, u_prev):
   # return normal_log_prob(x_t, x_prev + u_prev, true_stddev_transition)
@@ -99,15 +106,14 @@ def emiss_lp(y_t, x_t, u_t):
 net = torch.nn.Linear(4, 2)
 
 learning_rate = 1e-5
-# optimizer = torch.optim.Adam(net.parameters(), lr=learning_rate)
-optimizer = torch.optim.SGD(
-  list(net.parameters()) + [
-    # variational_log_stddev_emission,
-    # variational_log_stddev_transition,
-    variational_A
-  ],
-  lr=learning_rate
+opt_variables = (
+  list(net.parameters()) +
+  ([variational_log_stddev_emission] if variational_log_stddev_emission.requires_grad else []) +
+  ([variational_log_stddev_transition] if variational_log_stddev_transition.requires_grad else []) +
+  ([variational_A] if variational_A.requires_grad else [])
 )
+# optimizer = torch.optim.Adam(net.parameters(), lr=learning_rate)
+optimizer = torch.optim.SGD(opt_variables, lr=learning_rate)
 
 step = build_filter(1, emiss_lp, trans_lp, net, optimizer)
 
@@ -134,7 +140,8 @@ for t in range(n_steps):
   # No control for now
   u_t = Variable(torch.FloatTensor([0.0]), requires_grad=False)
 
-  x_t = x_prev + u_prev + true_stddev_transition * np.random.randn()
+  # Sample from the true model
+  x_t = true_A * x_prev + u_prev + true_stddev_transition * np.random.randn()
   y_t = x_t + true_stddev_emission * np.random.randn()
 
   mu_t, s_t = step(y_t, u_prev, mu_prev, s_prev)
@@ -152,11 +159,15 @@ print(net.weight)
 print(net.bias)
 
 import matplotlib.pyplot as plt
+import matplotlib.gridspec as gridspec
 
 # Plot the filtering results
-plt.figure(figsize=(12, 6))
+plt.figure(figsize=(14, 6))
+gs = gridspec.GridSpec(6, 2)
+gs.update(hspace=0, wspace=0.3)
 
-plt.subplot(2, 1, 1)
+# plt.subplot(2, 1, 1)
+plt.subplot(gs[:3, 0])
 plt.plot(np.arange(n_steps + 1), [t[0] for t in Xs])
 plt.errorbar(
   np.arange(n_steps + 1),
@@ -165,36 +176,44 @@ plt.errorbar(
   yerr=[3 * np.exp(t[0]) for t in Ss]
 )
 plt.legend(['true x_t', 'posterior with 3 std. dev. error bars'])
-plt.title('latent space')
+plt.xlabel('latent space')
 
-plt.subplot(2, 1, 2)
+# plt.subplot(2, 1, 2)
+plt.subplot(gs[3:, 0])
 plt.plot(np.arange(n_steps + 1), [t[0] for t in Ys])
 plt.legend(['observed y_t'])
-plt.title('observed space')
+plt.ylabel('observed space')
 
 plt.suptitle('Dual estimation on a 1-d LDS model\nlearning rate = {}, emission stddev = {}, transition stddev = {}, A = {}'.format(learning_rate, true_stddev_emission, true_stddev_transition, true_A))
 
 # Plot the variational model params
-plt.figure(figsize=(12, 6))
+# plt.figure(figsize=(12, 6))
 
-plt.subplot(3, 1, 1)
+# plt.subplot(3, 1, 1)
+plt.subplot(gs[:2, 1])
 plt.plot(np.arange(n_steps + 1), variational_log_stddev_transitions_per_iter)
 plt.axhline(np.log(true_stddev_transition), color='grey', linestyle='dotted')
+if not variational_log_stddev_transition.requires_grad:
+  plt.annotate('OFF', xy=(15, 15), xycoords='axes points', color='grey', size=24)
 plt.legend(['variational estimate', 'ground truth'])
 plt.ylabel('transition noise\nlog std. dev.')
 
-plt.subplot(3, 1, 2)
+# plt.subplot(3, 1, 2)
+plt.subplot(gs[2:4, 1])
 plt.plot(np.arange(n_steps + 1), variational_log_stddev_emissions_per_iter)
 plt.axhline(np.log(true_stddev_emission), color='grey', linestyle='dotted')
+if not variational_log_stddev_emission.requires_grad:
+  plt.annotate('OFF', xy=(15, 15), xycoords='axes points', color='grey', size=24)
 plt.legend(['variational estimate', 'ground truth'])
 plt.ylabel('emission noise\nlog std. dev.')
 
-plt.subplot(3, 1, 3)
+# plt.subplot(3, 1, 3)
+plt.subplot(gs[4:, 1])
 plt.plot(np.arange(n_steps + 1), variational_A_per_iter)
 plt.axhline(true_A, color='grey', linestyle='dotted')
 plt.legend(['variational estimate', 'ground truth'])
 plt.ylabel('A')
 
-plt.suptitle('Variational model parameters per iteration\nlearning rate = {}, emission stddev = {}, transition stddev = {}, A = {}'.format(learning_rate, true_stddev_emission, true_stddev_transition, true_A))
+# plt.suptitle('Variational model parameters per iteration\nlearning rate = {}, emission stddev = {}, transition stddev = {}, A = {}'.format(learning_rate, true_stddev_emission, true_stddev_transition, true_A))
 
 plt.show()
